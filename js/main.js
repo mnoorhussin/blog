38af8d56635bb3b10d80cd1ee24860951b6d7904
Mustafa Noorhussin <mu88ali@gmail.com>
2019-05-17 15:49:59 +0200

    deleted the function for the loader

--- js/main.js
+++
@@ -1,7 +0,0 @@
-setTimeout(function() {
-  var overlay = document.getElementById('overlay');
-  overlay.classList.add('hide');
-  setTimeout (function (){
-    overlay.remove()
-  }, 1000)
-}, 1000)
